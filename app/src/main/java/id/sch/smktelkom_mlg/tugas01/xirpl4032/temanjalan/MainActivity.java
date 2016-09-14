ca03b1e8dbf37701d6fccd3a3313fc30541bce3c
Via alviola <viaalviola@gmail.com>
2016-09-14 19:15:22 +0700

    commit 2 : deklarasi dan inisialisasi variabel

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4032/temanjalan/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl4032/temanjalan/MainActivity.java
@@ -2,12 +2,38 @@ package id.sch.smktelkom_mlg.tugas01.xirpl4032.temanjalan;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etAlamat, etAlamat2;
+    RadioButton rbM, rbS, rbH, rbR;
+    Button buttonOK;
+    TextView tvHasil;
+    CheckBox cbM, cbR, cbT, cbL;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etAlamat = (EditText) findViewById(R.id.editTextAlamat);
+        etAlamat2 = (EditText) findViewById(R.id.editTextAlamat2);
+        rbM = (RadioButton) findViewById(R.id.radioButtonM);
+        rbS = (RadioButton) findViewById(R.id.radioButtonS);
+        rbR = (RadioButton) findViewById(R.id.radioButtonR);
+        rbH = (RadioButton) findViewById(R.id.radioButtonH);
+        buttonOK = (Button) findViewById(R.id.buttonOK);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        cbM = (CheckBox) findViewById(R.id.checkBoxM);
+        cbR = (CheckBox) findViewById(R.id.checkBoxR);
+        cbT = (CheckBox) findViewById(R.id.checkBoxTU);
+        cbL = (CheckBox) findViewById(R.id.checkBoxL);
 
     }
+}
